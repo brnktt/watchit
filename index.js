da691ee622bfe16451994756267cf0b40ede8599
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-03-10 00:06:47 +0100

    initialized project + using a chokidar with debounce

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+
+const start = debounce(() => {
+  console.log("Starting users program");
+}, 100);
+
+chokidar
+  .watch(".")
+  .on("add", start)
+  .on("change", () => console.log("File changed"))
+  .on("unlink", () => console.log("File Unlinked"));
